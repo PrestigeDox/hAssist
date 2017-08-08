1b28fbbd105cdd290ac48d845f54a197fe758ea5
PrestigeDox <themutantreaper@gmail.com>
2017-08-08 12:34:14 +0100

    update main.c
    New incorrect banner, login system introduced. Credits clearer. Functions made neater.

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 // printf(WHITE"["RED"-"WHITE"] "GREEN"\n"COLOUR_RESET); ---> FALSE RED
 // printf(WHITE"["GREEN"+"WHITE"] "GREEN"\n"COLOUR_RESET); ---> TRUE GREEN
 
+//TO INCLUDE:
+//Reverse Netcat Shell {--}
+//Reverse Bash Shell   {--}
+//Calculator
+//Root via SSH
+//Write to file
+//Auto botnet setup  {--}
+//Auto scanner setup {--}
+//Scanner/Net download from site
+//Dox Generator {--}
+//Dox to file   {--}
+
+
 #include <stdio.h> //Including all needed modules
 #include <unistd.h>
 
@@ -14,6 +27,13 @@
 #define CYAN "\x1B[36m"
 #define WHITE "\x1B[37m"
 #define COLOUR_RESET "\x1b[0m" //Enabling colour reset to original server colour
+
+//////////////////
+char Username[50] = "prestige";
+char Password[50] = "dox";
+char UsernameAttempt[50];
+char PasswordAttempt[50];
+//////////////////
 
 int choice;
 int passwordSize;
@@ -85,15 +105,64 @@ void passwordGenerator() {
 }
 
 int contributerList() {
-	printf(WHITE"["GREEN"+"WHITE"] "GREEN"SKYPE ~ PrestigeDox\n"COLOUR_RESET);
-	sleep(1);
-	printf(WHITE"["GREEN"+"WHITE"] "GREEN"TWITTER ~ @PrestigeDox\n"COLOUR_RESET);
-	sleep(1);
-	printf(WHITE"["GREEN"+"WHITE"] "GREEN"DISCORD ~ PrestigeDox#2258\n"COLOUR_RESET);
-	sleep(1);
+	printf(WHITE"["GREEN"+"WHITE"] "GREEN"CONTACT:\n"COLOUR_RESET);
+	printf(WHITE"["RED"-"WHITE"] "GREEN"SKYPE ~ PrestigeDox\n"COLOUR_RESET);
+	printf(WHITE"["RED"-"WHITE"] "GREEN"TWITTER ~ @PrestigeDox\n"COLOUR_RESET);
+	printf(WHITE"["RED"-"WHITE"] "GREEN"DISCORD ~ PrestigeDox#2258\n"COLOUR_RESET);
+	printf("\n");
+	printf(WHITE"["GREEN"+"WHITE"] "GREEN"CREDITS:\n"COLOUR_RESET);
+	printf(WHITE"["RED"-"WHITE"] "GREEN"CODING: PrestigeDox\n"COLOUR_RESET);
+	printf(WHITE"["RED"-"WHITE"] "GREEN"INCORRECT BANNER: Entity.cs, Sprite\n"COLOUR_RESET);
+	sleep(5);
+	clearPage();
 }
 
 int main() {
+	clearPage(); //Clear page before hand to make sure everything is neat and tidy
+
+	printf(WHITE"["GREEN"+"WHITE"] "GREEN"ENTER USERNAME:\n>>> "COLOUR_RESET);
+	scanf("%s", UsernameAttempt);
+	if(strcmp(UsernameAttempt, Username) == 0) {
+		printf(WHITE"["GREEN"+"WHITE"] "GREEN"VALID USERNAME!\n"COLOUR_RESET);
+		sleep(2);
+		clearPage();
+
+		printf(WHITE"["GREEN"+"WHITE"] "GREEN"ENTER PASSWORD:\n>>> "COLOUR_RESET);
+		scanf("%s", PasswordAttempt);
+		if(strcmp(PasswordAttempt, Password) == 0) {
+			printf(WHITE"["GREEN"+"WHITE"] "GREEN"VALID PASSWORD!\n"COLOUR_RESET);
+			sleep(2);
+			clearPage();
+		}
+
+		else {
+			printf("\x1b[1;31m              _________                   _______  \n");
+	        printf("\x1b[1;33m    _-----____/   ========================|______| \n");
+	        printf("\x1b[1;32m    |           ______________/                    \n");
+	        printf("\x1b[1;34m    |    ___--_/(_)       ^                        \n");
+	        printf("\x1b[1;35m    |___ ---                                       \n");
+	        printf(GREEN"Get of this hAssist!\n");
+	        printf(GREEN"This is kept private and access is only for the owner of this server\n"COLOUR_RESET);
+	        sleep(3);
+	        clearPage();
+	        return 0;
+		}
+		
+	}
+
+	else {
+		printf("\x1b[1;31m              _________                   _______  \n");
+        printf("\x1b[1;33m    _-----____/   ========================|______| \n");
+        printf("\x1b[1;32m    |           ______________/                    \n");
+        printf("\x1b[1;34m    |    ___--_/(_)       ^                        \n");
+        printf("\x1b[1;35m    |___ ---                                       \n");
+        printf(GREEN"Get of this hAssist!\n");
+        printf(GREEN"This is kept private and access is ONLY for the owner of this server\n"COLOUR_RESET);
+        sleep(3);
+	    clearPage();
+        return 0;
+	}
+
 	printf(WHITE"["GREEN"+"WHITE"] "GREEN"Welcome to hAssist!\n\n"COLOUR_RESET);
 	sleep(1);
 
